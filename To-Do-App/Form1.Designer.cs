61c7029399511f5b2be21fe9e6678a381adbb653
Nino Meier <nino.meier@bene-edu.ch>
2023-11-23 08:59:24 +0100

    Add project files.

---
+++ To-Do-App/Form1.Designer.cs
@@ -0,0 +1,313 @@
+﻿namespace To_Do_App
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            tabControl1 = new TabControl();
+            tabPage1 = new TabPage();
+            vScrollBar1 = new VScrollBar();
+            tabPage2 = new TabPage();
+            vScrollBar2 = new VScrollBar();
+            tabPage3 = new TabPage();
+            vScrollBar3 = new VScrollBar();
+            tabPage4 = new TabPage();
+            vScrollBar4 = new VScrollBar();
+            monthCalendar1 = new MonthCalendar();
+            button1 = new Button();
+            label1 = new Label();
+            textBox1 = new TextBox();
+            textBox2 = new TextBox();
+            label2 = new Label();
+            radioButton1 = new RadioButton();
+            radioButton2 = new RadioButton();
+            radioButton3 = new RadioButton();
+            dateTimePicker1 = new DateTimePicker();
+            label3 = new Label();
+            tabControl1.SuspendLayout();
+            tabPage1.SuspendLayout();
+            tabPage2.SuspendLayout();
+            tabPage3.SuspendLayout();
+            tabPage4.SuspendLayout();
+            SuspendLayout();
+            // 
+            // tabControl1
+            // 
+            tabControl1.Controls.Add(tabPage1);
+            tabControl1.Controls.Add(tabPage2);
+            tabControl1.Controls.Add(tabPage3);
+            tabControl1.Controls.Add(tabPage4);
+            tabControl1.Location = new Point(0, 45);
+            tabControl1.Name = "tabControl1";
+            tabControl1.SelectedIndex = 0;
+            tabControl1.Size = new Size(1052, 415);
+            tabControl1.TabIndex = 0;
+            // 
+            // tabPage1
+            // 
+            tabPage1.Controls.Add(vScrollBar1);
+            tabPage1.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point);
+            tabPage1.Location = new Point(4, 29);
+            tabPage1.Name = "tabPage1";
+            tabPage1.Padding = new Padding(3);
+            tabPage1.Size = new Size(1044, 382);
+            tabPage1.TabIndex = 0;
+            tabPage1.Text = "All";
+            tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // vScrollBar1
+            // 
+            vScrollBar1.Location = new Point(1018, 0);
+            vScrollBar1.Name = "vScrollBar1";
+            vScrollBar1.Size = new Size(26, 386);
+            vScrollBar1.TabIndex = 2;
+            // 
+            // tabPage2
+            // 
+            tabPage2.Controls.Add(vScrollBar2);
+            tabPage2.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point);
+            tabPage2.Location = new Point(4, 29);
+            tabPage2.Name = "tabPage2";
+            tabPage2.Padding = new Padding(3);
+            tabPage2.Size = new Size(1044, 382);
+            tabPage2.TabIndex = 1;
+            tabPage2.Text = "School";
+            tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // vScrollBar2
+            // 
+            vScrollBar2.Location = new Point(1018, 0);
+            vScrollBar2.Name = "vScrollBar2";
+            vScrollBar2.Size = new Size(26, 386);
+            vScrollBar2.TabIndex = 1;
+            // 
+            // tabPage3
+            // 
+            tabPage3.Controls.Add(vScrollBar3);
+            tabPage3.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point);
+            tabPage3.Location = new Point(4, 29);
+            tabPage3.Name = "tabPage3";
+            tabPage3.Size = new Size(1044, 382);
+            tabPage3.TabIndex = 2;
+            tabPage3.Text = "Work";
+            tabPage3.UseVisualStyleBackColor = true;
+            // 
+            // vScrollBar3
+            // 
+            vScrollBar3.Location = new Point(1018, 0);
+            vScrollBar3.Name = "vScrollBar3";
+            vScrollBar3.Size = new Size(26, 386);
+            vScrollBar3.TabIndex = 2;
+            // 
+            // tabPage4
+            // 
+            tabPage4.Controls.Add(vScrollBar4);
+            tabPage4.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point);
+            tabPage4.Location = new Point(4, 29);
+            tabPage4.Name = "tabPage4";
+            tabPage4.Size = new Size(1044, 382);
+            tabPage4.TabIndex = 3;
+            tabPage4.Text = "Home";
+            tabPage4.UseVisualStyleBackColor = true;
+            // 
+            // vScrollBar4
+            // 
+            vScrollBar4.Location = new Point(1018, 0);
+            vScrollBar4.Name = "vScrollBar4";
+            vScrollBar4.Size = new Size(26, 386);
+            vScrollBar4.TabIndex = 2;
+            // 
+            // monthCalendar1
+            // 
+            monthCalendar1.CalendarDimensions = new Size(1, 2);
+            monthCalendar1.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point);
+            monthCalendar1.Location = new Point(1064, 74);
+            monthCalendar1.Name = "monthCalendar1";
+            monthCalendar1.TabIndex = 1;
+            // 
+            // button1
+            // 
+            button1.Font = new Font("Segoe UI", 28.2F, FontStyle.Bold, GraphicsUnit.Point);
+            button1.Location = new Point(7, 465);
+            button1.Name = "button1";
+            button1.Size = new Size(117, 63);
+            button1.TabIndex = 2;
+            button1.Text = "+";
+            button1.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI", 18F, FontStyle.Bold, GraphicsUnit.Point);
+            label1.Location = new Point(146, 482);
+            label1.MaximumSize = new Size(1000, 50);
+            label1.Name = "label1";
+            label1.Size = new Size(103, 41);
+            label1.TabIndex = 3;
+            label1.Text = "To-Do";
+            // 
+            // textBox1
+            // 
+            textBox1.Font = new Font("Segoe UI", 13.8F, FontStyle.Regular, GraphicsUnit.Point);
+            textBox1.ForeColor = SystemColors.AppWorkspace;
+            textBox1.Location = new Point(293, 487);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(415, 38);
+            textBox1.TabIndex = 4;
+            textBox1.Text = "Titel";
+            // 
+            // textBox2
+            // 
+            textBox2.BackColor = Color.White;
+            textBox2.ForeColor = Color.Gainsboro;
+            textBox2.Location = new Point(13, 537);
+            textBox2.Multiline = true;
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(1039, 249);
+            textBox2.TabIndex = 5;
+            textBox2.Text = "Beschreibung max. 2000 Zeichen";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Segoe UI", 18F, FontStyle.Bold, GraphicsUnit.Point);
+            label2.Location = new Point(1064, 537);
+            label2.MaximumSize = new Size(1000, 50);
+            label2.Name = "label2";
+            label2.Size = new Size(163, 41);
+            label2.TabIndex = 6;
+            label2.Text = "Kategorie:";
+            // 
+            // radioButton1
+            // 
+            radioButton1.AutoSize = true;
+            radioButton1.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            radioButton1.Location = new Point(1064, 581);
+            radioButton1.Name = "radioButton1";
+            radioButton1.Size = new Size(93, 32);
+            radioButton1.TabIndex = 7;
+            radioButton1.TabStop = true;
+            radioButton1.Text = "School";
+            radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // radioButton2
+            // 
+            radioButton2.AutoSize = true;
+            radioButton2.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            radioButton2.Location = new Point(1214, 581);
+            radioButton2.Name = "radioButton2";
+            radioButton2.Size = new Size(80, 32);
+            radioButton2.TabIndex = 8;
+            radioButton2.TabStop = true;
+            radioButton2.Text = "Work";
+            radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // radioButton3
+            // 
+            radioButton3.AutoSize = true;
+            radioButton3.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            radioButton3.Location = new Point(1351, 581);
+            radioButton3.Name = "radioButton3";
+            radioButton3.Size = new Size(86, 32);
+            radioButton3.TabIndex = 9;
+            radioButton3.TabStop = true;
+            radioButton3.Text = "Home";
+            radioButton3.UseVisualStyleBackColor = true;
+            // 
+            // dateTimePicker1
+            // 
+            dateTimePicker1.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            dateTimePicker1.Location = new Point(1058, 619);
+            dateTimePicker1.Name = "dateTimePicker1";
+            dateTimePicker1.Size = new Size(373, 34);
+            dateTimePicker1.TabIndex = 10;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.BackColor = Color.Cyan;
+            label3.Font = new Font("Segoe UI", 18F, FontStyle.Bold, GraphicsUnit.Point);
+            label3.Location = new Point(0, 0);
+            label3.Name = "label3";
+            label3.Size = new Size(1490, 41);
+            label3.TabIndex = 11;
+            label3.Text = "To-Do                                                                                                                                by Max and Nino               \r\n";
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1480, 856);
+            Controls.Add(label3);
+            Controls.Add(dateTimePicker1);
+            Controls.Add(radioButton3);
+            Controls.Add(radioButton2);
+            Controls.Add(radioButton1);
+            Controls.Add(label2);
+            Controls.Add(textBox2);
+            Controls.Add(textBox1);
+            Controls.Add(label1);
+            Controls.Add(button1);
+            Controls.Add(monthCalendar1);
+            Controls.Add(tabControl1);
+            Name = "Form1";
+            StartPosition = FormStartPosition.CenterScreen;
+            Tag = "";
+            Text = "Form1";
+            tabControl1.ResumeLayout(false);
+            tabPage1.ResumeLayout(false);
+            tabPage2.ResumeLayout(false);
+            tabPage3.ResumeLayout(false);
+            tabPage4.ResumeLayout(false);
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private TabControl tabControl1;
+        private TabPage tabPage1;
+        private TabPage tabPage2;
+        private TabPage tabPage3;
+        private TabPage tabPage4;
+        private MonthCalendar monthCalendar1;
+        private VScrollBar vScrollBar1;
+        private VScrollBar vScrollBar2;
+        private VScrollBar vScrollBar3;
+        private VScrollBar vScrollBar4;
+        private Button button1;
+        private Label label1;
+        private TextBox textBox1;
+        private TextBox textBox2;
+        private Label label2;
+        private RadioButton radioButton1;
+        private RadioButton radioButton2;
+        private RadioButton radioButton3;
+        private DateTimePicker dateTimePicker1;
+        private Label label3;
+    }
+}
---
+++ To-Do-App/Form1.cs
@@ -0,0 +1,10 @@
+namespace To_Do_App
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
